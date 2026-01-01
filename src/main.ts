db1d7463c67c21e81f6b652c4dc8453dede9df7a
Truth <2488292668@qq.com>
2026-01-01 22:09:26 +0800

    feat(user): piana存储

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 import router from "./config/router.ts";
+import { createPinia } from 'pinia'
 import Antd from "ant-design-vue";
 import 'ant-design-vue/dist/reset.css';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
 const app = createApp(App)
+const pinia = createPinia()
 app.use(Antd)
+app.use(pinia)
+pinia.use(piniaPluginPersistedstate);
 app.use(router)
 app.mount('#app')
