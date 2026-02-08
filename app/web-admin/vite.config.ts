22abbdc9bc5256531a89bdc44db60442e08618c5
Truth <2488292668@qq.com>
2026-02-08 18:37:46 +0800

    :heavy_plus_sign: 新增unplugin-vue-components依赖

--- app/web-admin/vite.config.ts
+++ app/web-admin/vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
+    plugins: [vue(),Components({
+        dts: "src/components.d.ts",
+        resolvers: [
+            AntDesignVueResolver({
+                importStyle: false, // css in js
+            }),
+        ],
+    })],
+    resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'), // 设置 @ 指向 src 目录
         },
